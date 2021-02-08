41405c778339ff6d3a0adc03ccb90439ddc1a340
Jaegoomon <jaegoo1199@gmail.com>
2021-02-08 18:47:22 +0900

    파일 포맷팅

--- hooks/useInput.js
+++ hooks/useInput.js
@@ -1,13 +1,12 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback } from 'react';
 
 const useInput = (initialValue = null) => {
-    const [value, setValue] = useState(initialValue);
-    const handler = useCallback((e) => {
-        setValue(e.target.value);
-    }, [])
+  const [value, setValue] = useState(initialValue);
+  const handler = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
 
-    return [value, handler];
-}
-
+  return [value, handler];
+};
 
 export default useInput;
--- sotre/configureStore.js
+++ sotre/configureStore.js
@@ -5,17 +5,17 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import reducer from '../reducers';
 
 const configureStore = () => {
-    const middlewares = [];
-    const enhancer = process.env.NODE_ENV === 'production'
-        ? compose(applyMiddleware(...middlewares))
-        : composeWithDevTools(applyMiddleware(...middlewares));
+  const middlewares = [];
+  const enhancer = process.env.NODE_ENV === 'production'
+    ? compose(applyMiddleware(...middlewares))
+    : composeWithDevTools(applyMiddleware(...middlewares));
 
-    const store = createStore(reducer, enhancer);
-    return store;
-}
+  const store = createStore(reducer, enhancer);
+  return store;
+};
 
 const wrapper = createWrapper(configureStore, {
-    debug: process.env.NODE_ENV === 'development'
+  debug: process.env.NODE_ENV === 'development',
 });
 
 export default wrapper;
